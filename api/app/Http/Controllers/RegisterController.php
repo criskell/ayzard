7be03f590c5180f2c3ad2dc39da4b7646788ad7b
Cristiano <cris.moraes.ds@gmail.com>
2022-12-16 20:13:32 -0300

    Retorna o ID do usuário no registro

--- api/app/Http/Controllers/RegisterController.php
+++ api/app/Http/Controllers/RegisterController.php
@@ -6,17 +6,18 @@ use Illuminate\Support\Facades\Hash;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Http\Resources\UserResource;
 
 class RegisterController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
 
-        return response()->noContent();
+        return new UserResource($user);
     }
 }
