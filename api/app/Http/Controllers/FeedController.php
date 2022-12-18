c263040b802caa3d37c648bd96136f9fd1ccb01a
Cristiano <cris.moraes.ds@gmail.com>
2022-12-18 16:59:47 -0300

    Compartilhamento de posts no feed

--- api/app/Http/Controllers/FeedController.php
+++ api/app/Http/Controllers/FeedController.php
@@ -3,21 +3,60 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\PostShare;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\DB;
 
 class FeedController extends Controller
 {
     public function show()
     {
-        $posts = Post::with([
-            'user',
-            'comments' => function ($query) {
-                $query->latest()->limit(15);
-            },
+        $sharedPosts = Post::select([
+            'posts.*',
+            'post_shares.created_at AS shared_at',
+            'post_shares.user_id AS shared_by_id',
+            DB::raw('"shared" as type')
         ])
-            ->withCount(['comments', 'likes'])
+            ->join('post_shares', 'post_shares.post_id', '=', 'posts.id');
+        $regularPosts = Post::select([
+            'posts.*',
+            DB::raw('NULL as shared_at'),
+            DB::raw('NULL as shared_by_id'),
+            DB::raw('"regular" as type')
+        ]);
+
+        $posts = $sharedPosts
+            ->unionAll($regularPosts)
+            ->orderByDesc(DB::raw('COALESCE(shared_at, created_at)'))
             ->paginate();
 
         return PostResource::collection($posts);
     }
+
+    // public function show()
+    // {
+    //     $sharedPosts = PostShare::with(['user', 'post'])
+    //         ->take(15)
+    //         ->latest()
+    //         ->get()
+    //         ->map(function ($share) {
+    //             $post = $share->post;
+    //             $post->entry_created_at = $post->shared_at = $share->created_at;
+    //             $post->shared_by = $share->user;
+    //             return $post;
+    //         });
+
+    //     $regularPosts = Post::with(['user'])
+    //         ->take(15)
+    //         ->latest()
+    //         ->get()
+    //         ->map(function ($post) {
+    //             $post->entry_created_at = $post->created_at;
+    //             return $post;
+    //         });
+
+    //     $posts = $regularPosts->merge($sharedPosts)->sortByDesc('entry_created_at');
+
+    //     return PostResource::collection($posts);
+    // }
 }
--- api/app/Http/Resources/PostResource.php
+++ api/app/Http/Resources/PostResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Models\User;
 
 class PostResource extends JsonResource
 {
@@ -10,11 +11,19 @@ class PostResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'type' => $this->type ?? 'regular',
             'content' => $this->content,
             'comments_count' => $this->whenCounted('comments'),
             'likes_count' => $this->whenCounted('likes'),
+            'shares_count' => $this->whenCounted('shares'),
             'comments' => CommentResource::collection($this->whenLoaded('comments')),
             'user' => new UserResource($this->whenLoaded('user')),
+            'shared_at' => $this->whenNotNull($this->shared_at),
+            'shared_by' => $this->when(
+                $this->shared_by_id,
+                fn () =>
+                    new UserResource(User::find($this->shared_by_id))
+            ),
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
