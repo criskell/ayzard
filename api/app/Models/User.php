e7fceb492da7788041e06c0aa8dbcf0f9b92ac52
Cristiano <cris.moraes.ds@gmail.com>
2022-12-18 18:12:52 -0300

    Remover Laravel Sanctum

--- api/app/Models/User.php
+++ api/app/Models/User.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 use PHPOpenSourceSaver\JWTAuth\Contracts\JWTSubject;
 
 class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'name',
