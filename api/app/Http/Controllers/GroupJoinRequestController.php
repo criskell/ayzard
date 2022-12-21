e11f0f149cb4dfbf7ba10a7dcd4b7f75444cf63c
Cristiano <cris.moraes.ds@gmail.com>
2022-12-20 21:17:53 -0300

    Adicionar estrutura para pedidos de entrada em grupos

---
+++ api/app/Http/Controllers/GroupJoinRequestController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\GroupJoinRequest;
+use Illuminate\Http\Request;
+
+class GroupJoinRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\GroupJoinRequest  $groupJoinRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(GroupJoinRequest $groupJoinRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\GroupJoinRequest  $groupJoinRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, GroupJoinRequest $groupJoinRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\GroupJoinRequest  $groupJoinRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(GroupJoinRequest $groupJoinRequest)
+    {
+        //
+    }
+}
---
+++ api/database/migrations/2022_12_21_001536_create_group_join_requests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_join_requests', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['pending', 'approved', 'refused']);
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('group_id')->constrained('groups');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_join_requests');
+    }
+};
